157fce64d5373cb2d2c87d5cbaea997b450e23b7
Zachary Yocum <zachary.yocum@me.com>
2017-04-12 20:54:14 -0400

    add script and fill README

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# TMDb
+# Query The Movie Database (TMDb) for actors who worked on the same movies/shows
+
+## TMDb API
+This script relies on the [TMDb API](https://www.themoviedb.org), so if you don't have an account with TMDb, you need to create one and [request an API key](https://www.themoviedb.org/settings/api).
+
+## Usage
+
+	./tmdb.py -h
+	usage: tmdb.py [-h] [-k API_KEY] queries [queries ...]
+	
+	Search TMDb for common credits between cast/crew
+	
+	positional arguments:
+	  queries               Query strings
+	
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  -k API_KEY, --api-key API_KEY
+
+
+**Note:** If you don't want to enter your API key every time, you can set a `TMDB_USER_KEY` environment variable and the script will use that instead:
+
+	$ echo "export TMDB_USER_KEY=<your-key>" >> ~/.bash_profile
+	$ . ~/.bash_profile
+
+## Example
+
+If you supply a single query you will get a complete credit list for that person:
+
+	$ ./tmdb.py 'Nathan Fillion'
+	{
+	    "persons": [
+	        "Nathan Fillion"
+	    ],
+	    "common_credits": [
+	        "Being Canadian",
+	        "Blast from the Past",
+	        "Browncoats Unite: Firefly 10th Anniversary Special",
+	        "Buffy the Vampire Slayer",
+	        "Castle",
+	        ...
+	    ]
+	}
+
+If you supply multiple queries you will get credit results that all of those persons have in common:
+	
+	./tmdb.py 'Nathan Fillion' 'Gina Torres'
+	{
+	    "persons": [
+	        "Gina Torres",
+	        "Nathan Fillion"
+	    ],
+	    "common_credits": [
+	        "Browncoats Unite: Firefly 10th Anniversary Special",
+	        "Castle",
+	        "Con Man",
+	        "Firefly",
+	        "Justice League",
+	        "Serenity"
+	    ]
+	}
---
+++ tmdb.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+"""Search TMDb for common credits between cast/crew"""
+
+import os
+import json
+import requests
+import sys
+import urllib
+
+from getpass import getpass
+from pprint import pprint
+
+KEY = os.environ.get('TMDB_USER_KEY')
+BASE_URL = 'https://api.themoviedb.org/3'
+
+def search(facet, query, **params):
+    """Query TMDb to find the ID of something"""
+    # https://developers.themoviedb.org/3/search
+    facets = {
+        'company',
+        'collection',
+        'keyword',
+        'movie',
+        'multi',
+        'person',
+        'tv'
+    }
+    if facet not in facets:
+        message = 'invalid facet "{}" (must be one of {})'
+        raise ValueError(message.format(facet, sorted(facets)))
+    query = urllib.parse.quote_plus(query)
+    params['query'] = query
+    url = '/'.join([BASE_URL, 'search', facet])
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        response = json.loads(response.content.decode(response.encoding))
+        if response.get('results'):
+            top_result = response['results'][0]
+            return {
+                'id': top_result['id'],
+                'name': top_result['name']
+            }
+    message = (
+        'Error {}: failed to get results for query '
+        '"search/{}&query={}"'
+    )
+    print(message.format(response.status_code, facet, query), file=sys.stderr)
+
+def credits(person_id, **params):
+    """Generate a lits of combined TV/movie credits for a person ID"""
+    # https://developers.themoviedb.org/3/people/get-person-combined-credits
+    url = '/'.join([BASE_URL, 'person', str(person_id), 'combined_credits'])
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        response = json.loads(response.content.decode(response.encoding))
+        for key in 'cast', 'crew':
+            for credit in response[key]:
+                yield {
+                    'title': (credit.get('title') or credit.get('name')),
+                    'id': credit['id']
+                }
+    else:
+        message = (
+            'Error {}: failed to get credits for person ID '
+            '"person/{}/combined_credits"'
+        )
+        print(message.format(response.status_code, person_id), file=sys.stderr)
+
+def common_credits(*queries, **params):
+    """Find the intersection of credits from a list of person queries"""
+    persons = {}
+    for query in queries:
+        person = search('person', query, **params)
+        if person:
+            person['credits'] = {
+                c['title'] for c in credits(person['id'], **params)
+            }
+            persons[person['id']] = person
+    common_credits = set.intersection(
+        *(p['credits'] for p in persons.values())
+    )
+    return {
+        'persons': sorted(p['name'] for p in persons.values()),
+        'common_credits': sorted(common_credits)
+    }
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'query',
+        nargs='+',
+        help="Names of persons to query"
+    )
+    parser.add_argument(
+        '-k', '--api-key',
+        default=KEY
+    )
+    args = parser.parse_args()
+    key = args.api_key or getpass(prompt='TMDb API key: ')
+    print(
+        json.dumps(
+            common_credits(*args.query, api_key=key),
+            ensure_ascii=False,
+            indent=4
+        )
+    )
